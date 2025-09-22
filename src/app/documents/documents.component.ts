f382247d8dc39149da7e467b762e6dd0df6047cb
emmanuel <emmanuelnola4@gmail.com>
2025-09-22 21:52:40 +0200

    Mes modifs sur documents

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,9 +1,6 @@
 
 
 import { Component, OnInit, signal } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Filesystem, Directory } from '@capacitor/filesystem'; // Pour stocker les fichiers sur mobile
-import { FileOpener } from '@capacitor-community/file-opener';  // Pour ouvrir le PDF
 import { ToastController, IonicModule } from '@ionic/angular';  // Pour les toasts
 import { DocumentsService, MyDocument } from '../services/documents.service';
 import { Capacitor } from '@capacitor/core';
@@ -23,6 +20,7 @@ export class DocumentsComponent implements OnInit {
   // Signal pour stocker et mettre à jour la liste des documents de manière réactive
   documents: MyDocument[] = [];
    loading: boolean = true;
+   pdfUrl:string="";
 
   constructor(
     private documentsService: DocumentsService,
@@ -38,70 +36,26 @@ export class DocumentsComponent implements OnInit {
          error: async (err) => {
            this.loading = false;
            console.error(err);
-           await this.showToast('Impossible de charger les documents', 'danger');
+
          }
        });
-   }
+  }
 
 
    // Fonction pour télécharger et ouvrir un document PDF
    async download(doc: MyDocument) {
-     try {
-       this.loading = false;
-       //  Télécharger le PDF depuis l'URL
-       console.log(`${environment.apiLink}${doc.field_fichier}`);
-       console.log(`${environment.apiLink}${doc.field_fichier}`);
-       const response = await fetch(`${environment.apiLink}${doc.field_fichier}`);
-
-       console.log('bonjour');
-       if (!response.ok) throw new Error('Impossible de télécharger le PDF');
-       const blob = await response.blob();
-       console.log('bonjour 2');
-       const arrayBuffer = await blob.arrayBuffer();
-       const base64Data = this.arrayBufferToBase64(arrayBuffer); // Convertir en base64 pour Capacitor
-
-       //  Choisir le répertoire selon la plateforme
-       const directory = Capacitor.getPlatform() === 'ios' ? Directory.Documents : Directory.External;
-
-       //  Générer un nom unique pour éviter d'écraser d'autres fichiers
-       const timestamp = new Date().getTime();
-       const fileName = `${doc.title}_${timestamp}.pdf`;
-
-       //  Écrire le fichier sur le mobile
-       const savedFile = await Filesystem.writeFile({
-         path: fileName,
-         data: base64Data,
-         directory: directory,
-         recursive: true
-       });
-
-       //  Ouvrir le PDF avec l'application PDF par défaut
-       await FileOpener.open({
-         filePath: savedFile.uri,
-         contentType: 'application/pdf'
-       });
-
-       //  Afficher un toast vert pour succès
-       await this.showToast(`${doc.title} téléchargé et ouvert !`, 'success');
-
-     } catch (error) {
-        this.loading = false;
-       console.error(error);
-       // Afficher un toast rouge si erreur
-       await this.showToast(`Erreur lors du téléchargement de ${doc.title}`, 'danger');
-     }
-   }
-
-   // Convertit un ArrayBuffer en base64 pour Capacitor Filesystem
-   arrayBufferToBase64(buffer: ArrayBuffer) {
-     let binary = '';
-     const bytes = new Uint8Array(buffer);
-     bytes.forEach(b => binary += String.fromCharCode(b));
-     return btoa(binary);
-   }
+      this.pdfUrl=`${environment.apiLink}${doc.field_fichier}`;
+    //   await  this.documentsService.downloadAndOpenPdf(  `${environment.apiLink}${doc.field_fichier}`,doc.title);
+        /*   if (success) {
+             await this.showToast(`${doc.title} téléchargé et ouvert !`, 'success');
+           } else {
+             console.log('❌ Erreur lors du téléchargement');
+              await this.showToast(`Erreur lors du téléchargement de ${doc.title}`, 'danger');
+           }*/
+ //  }
 
    // Affiche un toast avec un message et une couleur (success/danger/primary)
-   async showToast(message: string, color: string = 'primary') {
+/*   async showToast(message: string, color: string = 'primary') {
      const toast = await this.toastController.create({
        message,
        duration: 2000, // Durée du toast 2 secondes
@@ -110,4 +64,12 @@ export class DocumentsComponent implements OnInit {
      });
      await toast.present();
    }
+
+
+   private getLastSubstring(path: string): string {
+     if (!path) return "";
+     const parts = path.split('/');
+     return parts[parts.length - 1]; // la dernière sous-chaîne
+   }*/
  }
+
--- src/app/services/documents.service.ts
+++ src/app/services/documents.service.ts
@@ -1,9 +1,14 @@
+import { ToastController } from '@ionic/angular';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Http } from '@capacitor-community/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
+import { Capacitor } from '@capacitor/core';
 
+import { Filesystem, Directory } from '@capacitor/filesystem'; // Pour stocker les fichiers sur mobile
+import { FileOpener } from '@capacitor-community/file-opener';  // Pour ouvrir le PDF
 
 export interface MyDocument {
   title: string;
@@ -14,7 +19,7 @@ export interface MyDocument {
   providedIn: 'root'
 })
 export class DocumentsService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,  private toastController: ToastController) {}
       private apiUrl = environment.apiLink +'/api/documents';
 
     // Récupérer la liste des audios depuis l'API
@@ -22,7 +27,78 @@ export class DocumentsService {
       return this.http.get<MyDocument[]>(this.apiUrl);
     }
 
+    // Télécharge et sauvegarde le PDF
+    async downloadAndOpenPdf(fileUrl: string, fileName: string) {
+       try {
 
+           const isAndroid = Capacitor.getPlatform() === 'android';
+           // Sur Android, on met tout dans le sandbox, pas besoin de permission supplémentaire
+            // Sur iOS, Directory.Documents correspond au sandbox de l'app
+           const fileDirectory = isAndroid ? undefined : Directory.Documents;
+
+            await this.showToast(`debut du telechargement : `, 'primary');
+         // Télécharger le fichier
+               const result = await Http.downloadFile({
+                 url: fileUrl,
+                 filePath: fileName,
+                 fileDirectory: fileDirectory as Directory | undefined,
+               });
+              await this.showToast(`j'ai traversé le telechargement de: ${fileName}`, 'primary');
+                                                         await this.delay(3000);
+
+              if (result.path) {
+                           console.log('Fichier téléchargé ici :', result.path);
+
+                           // Ouvre le PDF
+                           await FileOpener.open({
+                             filePath: result.path,
+                             contentType: 'application/pdf',
+                           });
+
+                           await this.showToast(`Téléchargement terminé : ${fileName}`, 'success');
+                         } else {
+                           throw new Error('Impossible de télécharger le PDF');
+                         }
+                       }catch (error) {
+                              console.error('Erreur téléchargement PDF :', error);
+                              await this.showToast('Erreur lors du téléchargement', 'danger');
+                     }
+       }
+
+
+
+
+       // Convertir ArrayBuffer → Base64
+         private arrayBufferToBase64(buffer: ArrayBuffer): string {
+           let binary = '';
+           const bytes = new Uint8Array(buffer);
+           const len = bytes.byteLength;
+           for (let i = 0; i < len; i++) {
+             binary += String.fromCharCode(bytes[i]);
+           }
+           return btoa(binary);
+         }
+
+      private delay(ms: number) {
+         return new Promise(resolve => setTimeout(resolve, ms));
+       }
+
+      private async showToast(message: string, color: 'success' | 'danger' | 'primary' = 'primary') {
+          const toast = await this.toastController.create({
+            message,
+            duration: 3000,
+            color,
+            position: 'bottom',
+          });
+          await toast.present();
+        }
+
+       //le nom du document c'est la  derniere sous chaines de  chaine avec comme separateur \
+       private getLastSubstring(path: string): string {
+           if (!path) return "";
+           const parts = path.split('/');
+           return parts[parts.length - 1]; // la dernière sous-chaîne
+         }
 
 }
 
