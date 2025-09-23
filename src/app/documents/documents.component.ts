81d5e9a7c19c63680a1f9c786daac4b896696ab5
emmanuel <emmanuelnola4@gmail.com>
2025-09-23 12:22:04 +0200

    document modifié

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -49,46 +49,6 @@ export class DocumentsComponent implements OnInit {
    async download(doc: MyDocument) {
      this.pdfUrl=`${environment.apiLink}${doc.field_fichier}`;
      this.nomFichier= `${doc.title}.pdf`;}
-     /* this.downloading = true;
-          const url = `${environment.apiLink}${doc.field_fichier}`;
-          const fileName = `${doc.title}.pdf`;
 
-          const uri = await this.documentsService.downloadFile(url, fileName, (percent) => {
-            this.progress = percent;
-          });
-
-          this.downloading = false;
-          if (uri) {
-            console.log('PDF téléchargé :', uri);
-
-          }
-        }*/
-
-     // await  this.documentsService.downloadFile(  `${environment.apiLink}${doc.field_fichier}`,doc.title);}
-        /*   if (success) {
-             await this.showToast(`${doc.title} téléchargé et ouvert !`, 'success');
-           } else {
-             console.log('❌ Erreur lors du téléchargement');
-              await this.showToast(`Erreur lors du téléchargement de ${doc.title}`, 'danger');
-           }*/
- //  }
-
-   // Affiche un toast avec un message et une couleur (success/danger/primary)
-/*   async showToast(message: string, color: string = 'primary') {
-     const toast = await this.toastController.create({
-       message,
-       duration: 2000, // Durée du toast 2 secondes
-       color,
-       position: 'bottom'
-     });
-     await toast.present();
-   }
-
-
-   private getLastSubstring(path: string): string {
-     if (!path) return "";
-     const parts = path.split('/');
-     return parts[parts.length - 1]; // la dernière sous-chaîne
-   }*/
- }
+}
 
