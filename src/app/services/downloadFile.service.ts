c54357a273084e5d4dd815d9b6bb18502cac5a03
emmanuel <emmanuelnola4@gmail.com>
2025-09-27 18:32:18 +0200

    production 2

--- src/app/services/downloadFile.service.ts
+++ src/app/services/downloadFile.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
-
+import {  Directory } from '@capacitor/filesystem';
+// utile pour ancienne version import { Filesystem } from '@capacitor/filesystem';
+//utile pour la nouvelle version
+import writeFile from 'capacitor-blob-writer';
 
 export interface MyAudio {
   title: string;
@@ -43,14 +45,20 @@ export class DownloadFileService {
       this.progressCallback = callback;
     }
 
-    async downloadFile(audioUrl: string,fileName: string): Promise<string> {
+    async downloadFile(audioUrl: string,fileNom: string): Promise<string> {
+
+
        return new Promise((resolve, reject) => {
 
-
-         const xhr = new XMLHttpRequest();
+          const fileName = `${this.removeExtension(fileNom)}_${new Date().getTime()}.pdf`;
+          console.log(fileName);
+          const xhr = new XMLHttpRequest();
 
          xhr.open('GET', audioUrl, true);
          xhr.responseType = 'blob';
+
+           // Timeout (2.5 min) un ajout
+              xhr.timeout = 150000;
 
          //  VÉRITABLE GESTION DE LA PROGRESSION
                xhr.onprogress = (event) => {
@@ -73,34 +81,42 @@ export class DownloadFileService {
            console.log('Téléchargement terminé, statut:', xhr.status);
            if (xhr.status === 200) {
              try {
-               const blob = xhr.response;
+                 // j ai ahouté a l ancienne version as Blob
+               const blob = xhr.response as Blob;
                console.log('Taille du blob:', blob.size);
 
             // ✅ FORCER une progression à 80% pendant la conversion
              if (this.progressCallback) {
                  this.progressCallback(80);
              }
-
+              /* version précédente
                const base64Data = await this.blobToBase64(blob);
 
               // ✅ FORCER une progression à 90% pendant la sauvegarde
                if (this.progressCallback) {
                   this.progressCallback(90);
-                }
-
-               const result = await Filesystem.writeFile({
+                }*/
+              /*anciennce sauvegarde
+               const result = await writeFile({
                  path: fileName,
                  data: base64Data,
                  directory: Directory.Documents,
                  recursive: true
-               });
+               });*/
+
+             //nouvelle sauvegarde
+               const result = await writeFile({
+                  path: fileName,
+                   directory: Directory.Documents, // stable et sandboxé
+                    blob,
+                 });
 
                if (this.progressCallback) {
                  this.progressCallback(100); // Progression à 100%
                }
 
-              console.log('Fichier sauvegardé:', result.uri);
-              resolve(result.uri);
+              console.log('Fichier sauvegardé:', result);
+              resolve(result);
 
              } catch (error) {
                reject(error);
@@ -120,6 +136,15 @@ export class DownloadFileService {
          xhr.send();
        });
      }
+
+   //je veux enlever le .xxx de fin
+   private removeExtension(fileName: string): string {
+     const dotIndex = fileName.lastIndexOf('.');
+     if (dotIndex === -1) return fileName; // pas d'extension trouvée
+     return fileName.substring(0, dotIndex);
+   }
+
+
 
      // Convertir blob en base64
       private blobToBase64(blob: Blob): Promise<string> {
